8a0da4ea6551320a43389f04c107f5e120f2fbaa
taxguide <singhparash06@gmail.com>
2026-07-28 12:55:58 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'www.disnyplus.com/begin'
+author = 'www.disnyplus.com/begin'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "www.disnyplus.com/begin"
